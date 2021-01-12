074f4a13868226ae54b04025eff214c4f50c7332
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-01-12 06:31:54 -0500

    Updated the Instructions buttons on both pages

--- odds.js
+++ odds.js
@@ -65,7 +65,7 @@ $(document).ready(function(){
         });
     })
 
-    // Instructions Modal
+    // Odds Page Instructions Modal
     $("#instructionsButton").on("click", function (e) {
         e.preventDefault();
         
@@ -74,12 +74,11 @@ $(document).ready(function(){
         
         // Create unordered list of instructions
         var instructList = $('<ul>').appendTo('#oddsInstructionText');
-        var liStep1 = $('<li>').text('Click on the "Show Teams Playing" Button').appendTo(instructList);
-        var liStep2 = $('<li>').text('Review list of games by Visitor vs. Home teams').appendTo(instructList);
-        var liStep3 = $('<li>').text('Click on a listed Game').appendTo(instructList);
-        var liStep4 = $('<li>').text('View the Results of the Head-to-Head Odds').appendTo(instructList);
-        var liStep5 = $('<li>').text('Good Luck!').appendTo(instructList);
-        $('#oddsInstructionText').append(liStep1, liStep2, liStep3, liStep4, liStep5)
+            $('<li>').text('Click on the "Show Teams Playing" Button').appendTo(instructList);
+            $('<li>').text('Review list of games by Visitor vs. Home teams').appendTo(instructList);
+            $('<li>').text('Click on a listed Game').appendTo(instructList);
+            $('<li>').text('View the Results of the Head-to-Head Odds').appendTo(instructList);
+            $('<li>').text('Good Luck!').appendTo(instructList);
 
         // Display Instructions modal
         $("#modalInstructions").modal({
